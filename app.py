c1c3d347653dce26666f619b1fc41086cc93ef89
IgorDF02 <igordfernandes2010@hotmail.com>
2024-10-09 14:50:48 -0300

    Update app.py
    Adicionando informações para o Flask + FPDF possam gerar o PDF

--- app.py
+++ app.py
@@ -5,26 +5,50 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('form.html')  # Coloque o HTML em um arquivo chamado 'form.html'
+    return render_template('valores.html')  # Coloque o HTML em um arquivo chamado 'valores.html'
 
-@app.route('/gerar_pdf', methods=['POST'])
+@app.route('gerar_pdf', methods=['POST'])
 def gerar_pdf():
-    nome = request.form['nome']
-    email = request.form['email']
+    liquidez_g = request.form['liquidez_g']
+    liquidez_c = request.form['liquidez_c']
+    liquidez_s = request.form['liquidez_s']
+    liquidez_i = request.form['liquidez_i']
+    
+    mb = request.form['mb']
+    mo = request.form['mo']
+    ml = request.form['ml']
+    roe  = request.form['roe']
+    roa  = request.form['roa']
+    
+    comp_end = request.form['ecomp_end']
+    end_geral = request.form['end_geral']
+    part_cap = request.form['part_cap']
 
     # Criar o PDF
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
     
-    pdf.cell(200, 10, txt=f"Nome: {nome}", ln=True)
-    pdf.cell(200, 10, txt=f"Email: {email}", ln=True)
+    pdf.cell(200, 10, txt=f"Liquidez : {liquidez_g}", ln=True)
+    pdf.cell(200, 10, txt=f"Liquidez : {liquidez_c}", ln=True)
+    pdf.cell(200, 10, txt=f"Liquidez : {liquidez_g}", ln=True)
+    pdf.cell(200, 10, txt=f"Liquidez : {liquidez_c}", ln=True)
+
+    pdf.cell(200, 10, txt=f"mb: {mb}", ln=True)
+    pdf.cell(200, 10, txt=f"mo: {mo}", ln=True)
+    pdf.cell(200, 10, txt=f"ml: {ml}", ln=True)
+    pdf.cell(200, 10, txt=f"roe: {roe}", ln=True)
+    pdf.cell(200, 10, txt=f"roa: {roa}", ln=True)
+    
+    pdf.cell(200, 10, txt=f"comp_end: {comp_end}", ln=True)
+    pdf.cell(200, 10, txt=f"end_geral: {end_geral}", ln=True)
+    pdf.cell(200, 10, txt=f"part_cap: {part_cap}", ln=True)
 
     # Salvar o PDF
     pdf_file_path = "formulario.pdf"
     pdf.output(pdf_file_path)
 
-    return f"PDF gerado com sucesso! <a href='/{pdf_file_path}'>Baixar PDF</a>"
+    return f"PDF gerado com sucesso! <a href='{pdf_file_path}'>Baixar PDF</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
